96a4d1b9a655e6fec8b2df1c4ef1ad2b11830b61
Suyash Sonar <81332692+suyash1397@users.noreply.github.com>
2023-11-05 13:58:40 +0530

    Update DAA_3.py

--- DAA/DAA_3.py
+++ DAA/DAA_3.py
@@ -1,40 +1,19 @@
-# Function to solve the Fractional Knapsack problem
-def fractional_knapsack(items, capacity):
-    # Calculate the value-to-weight ratio for each item
-    for item in items:
-        item['ratio'] = item['value'] / item['weight']
-
-    # Sort the items by the value-to-weight ratio in descending order
-    items.sort(key=lambda x: x['ratio'], reverse=True)
-
-    total_value = 0.0
-    knapsack = []
-
-    for item in items:
-        if capacity >= item['weight']:
-            # If the entire item can be added, add it and update capacity
-            knapsack.append(item)
-            total_value += item['value']
-            capacity -= item['weight']
+class Item:
+    def __init__(self,profit,weight):
+        self.profit = profit
+        self.weight = weight
+def knapsac(w,arr):
+    finalvalue =0.0
+    arr.sort(key=lambda x:(x.profit/x.weight),reverse = True )
+    for i in arr:
+        if i.weight<=w:
+            w-=i.weight
+            finalvalue+=i.profit
         else:
-            # If only a fraction of the item can be added, add a fraction
-            fraction = capacity / item['weight']
-            knapsack.append({'name': item['name'], 'weight': item['weight'] * fraction, 'value': item['value'] * fraction})
-            total_value += item['value'] * fraction
+            finalvalue += i.profit*w / i.weight
             break
+    return finalvalue
 
-    return knapsack, total_value
-
-# Example usage
-if __name__ == "__main__":
-    items = [
-        {'name': 'item1', 'weight': 10, 'value': 60},
-        {'name': 'item2', 'weight': 20, 'value': 120},
-        {'name': 'item3', 'weight': 30, 'value': 130}
-    ]
-
-    max_capacity = 50
-
-    knapsack_items, max_value = fractional_knapsack(items, max_capacity)
-
-    print(f"Maximum value in the knapsack: {max_value}")
+arr = [Item(25,18),Item(24,15),Item(15,10)]
+w=20
+print(knapsac(w,arr))
